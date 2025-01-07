fa0d6712c813e3f318026fb783ce79ad3ae66e50
Reuben9027 <73296109+Reuben9027@users.noreply.github.com>
2025-01-07 10:08:46 +0800

    Cleaning up
    cleaning up

--- solutions/climbingworm/challenge.java
+++ solutions/climbingworm/challenge.java
@@ -11,21 +11,7 @@ class challenge {
         int a = s.nextInt();
         int b = s.nextInt();
         int c = s.nextInt();
-        // int d = a - b;
-        // int k = c;
         c -= a;
         System.out.printf("%.0f\n", c <= 0 ? 1 : 1 + Math.ceil((double) c / (a - b)));
-
-        // int r = 0;
-        // while (true) {
-        // k -= a;
-        // r += 1;
-        // if (k <= 0) {
-        // break;
-        // }
-        // k += b;
-        // }
-
-        // System.out.println(r);
     }
 }
